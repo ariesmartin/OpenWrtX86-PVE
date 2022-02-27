981ee59968c8b2f99d903e1dfcdeb37aba90caff
AriesMartin <38410625+ariesmartin@users.noreply.github.com>
2022-02-27 18:52:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/linkease/istore/tree/main/luci/luci-app-store package/store
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore/tree/main/luci/luci-app-store' >>feeds.conf.default
+
